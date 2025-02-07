3b8d0bf8bdeaf3ad619f058d97ed7071a9237709
Nicholas-Olsen <redbean74737@gmail.com>
2025-02-07 11:16:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase 설정
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyAzWxU0YWSogrIaE47fsFBG2PIMDiVBjSo",
     authDomain: "todays-best-recipe-signup.firebaseapp.com",
@@ -14,10 +17,6 @@
     measurementId: "G-LJRKJS7NY7"
   };
 
- // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
