12bf800e6f47aa5060ae920dafe2bf301451423a
cjming354 <jeamingg@gmail.com>
2025-02-12 14:39:29 +0900

    Update urls.py
    url추가 (koreanfood로)

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import home_view, main_view, signup_view, koreanfood, korean_food_list, signup, login
+from blog.views import home_view, main_view, signup_view, koreanfood, korean_food_list, signup, login,de_food,ko_food,ja_food,ch_food,we_food
 
 urlpatterns = [
     path('',home_view ,name='home'),
@@ -9,4 +9,9 @@ urlpatterns = [
     path('login/',login, name='login'),
     path('koreanfood/',koreanfood, name='koreanfood'),
     path('korean-food/', korean_food_list, name='korean_food_list'),
+    path('ko_food',ko_food ,name='ko_food'),
+    path('ja_food',ja_food ,name='ja_food'),
+    path('ch_food',ch_food ,name='ch_food'),
+    path('we_food',we_food ,name='we_food'),
+    path('de_food',de_food ,name='de_food'),
 ]
